4e7d0ead54108e70b0bb92a81f6c1b55674ea4a1
Rudra <rudraponkshe10c.vhs@gmail.com>
2022-01-16 20:41:12 +0530

    Added speedtest

--- user1/autofill.js
+++ user1/autofill.js
@@ -8,11 +8,18 @@ function setwaspd1(){
     let newwa1spd= prompt("Enter the number you'd like to add as WhatsApp speeddial")
     localStorage.setItem('waspd1', newwa1spd)
 }
-
+function setigsp1(){
+    let newigsp1= prompt("Enter the id of the Instagram account you want to add to your speedial")
+    localStorage.setItem('igspd1', newigsp1)
+}
 
 // Setting Speedial onload
 
 function setspddial1(){
     let phspdrtr1= localStorage.getItem('phpref1')
     document.getElementById('u1phsp').href= "callto:+91"+ phspdrtr1;
+
+
+    let waspdrtr1= localStorage.getItem("waspd1")
+    document.getElementById('u1wasp').href= "https://wa.me/+91"+ waspdrtr1;
 }
